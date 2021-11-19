c4be059b59741e6d3c0a1ac698eab86868433ec9
szabozsolt20 <szabo.zsolt.debrecen@gmail.com>
2021-11-19 22:14:08 +0100

    Kész

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,14 @@
+const converterPromise = (arr) => {
+    return new Promise((resolve, reject) => {
+        if (arr.every(item => typeof item === "string")) {
+            const arrUpperCase = arr.map(item => item.toLocaleUpperCase('hu-HU'));
+            resolve(arrUpperCase)
+        } else {
+            reject('Error: Not all elements are string type!')
+        }
+    });
+}
 
+converterPromise(['tibi', 4, 'illés']).then(console.log)
 
-
-
-
-
-// export default converterPromise;
+export default converterPromise;
